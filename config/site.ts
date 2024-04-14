651926792df1ea7ac49065d7a38b21abfba88f63
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2024-04-14 13:15:42 +0700

    Configure Next.js project settings

---
+++ config/site.ts
@@ -0,0 +1,6 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "SWD392 SU24",
+  description: "This is a demo for the SWD392 SU24.",
+}
---
+++ lib/schemas/user.schema.ts
@@ -0,0 +1,6 @@
+import * as z from "zod"
+
+export const UserSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+})
