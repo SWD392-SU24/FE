7e6877964f3f976aa6ce9d2b914dfd87539302da
L3oN007 <minhnhatt0908@gmail.com>
2024-04-25 14:16:31 +0700

    ci: implement environmental variable control for configuration

---
+++ env.ts
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "test", "production"]),
+  },
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().url(),
+    NEXT_PUBLIC_API_ENDPOINT: z.string().url(),
+  },
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  },
+  emptyStringAsUndefined: true,
+})
