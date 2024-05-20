f4496034d97e0b7d4e87076884fe8705bcf03956
L3oN007 <minhnhatt0908@gmail.com>
2024-05-20 18:33:07 +0700

    ci: switch package manager from npm to yarn

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,38 @@
-export type SiteConfig = typeof siteConfig
+import { Metadata } from "next"
 
-export const siteConfig = {
-  name: "SWD392 SU24",
-  description: "This is a demo for the SWD392 SU24.",
+export function constructMetadata({
+  title = {
+    default: "DentiCare - Click to Confident Smile ",
+    template: "%s | DentiCare - Click to Confident Smile",
+  },
+  description = "DentiCare is a platform designed to bridge the gap between customers seeking dental care services and private dental clinics in Vietnam. With the mission of simplifying the dental care experience, it aims to contribute to creating a community that provides comprehensive solutions, focusing on long-term health and the absolute satisfaction of customers.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: {
+    default: string
+    template: string
+  }
+  description?: string
+  image?: string
+  icons?: string
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title: title.default,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: title.default,
+      description,
+      images: [image],
+      creator: "@DentiCare",
+    },
+    icons,
+    metadataBase: new URL("https://denticare.com/"),
+  }
 }
